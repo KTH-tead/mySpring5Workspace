b0a1095ceff930449cafb591f15a95d34d29a97a
eddy_goott <kthhyung@gmail.com>
2021-05-26 17:31:35 +0900

    bootsrart

--- ex00/src/main/java/practice/controller/chap06/SampleController.java
+++ ex00/src/main/java/practice/controller/chap06/SampleController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import lombok.extern.log4j.Log4j;
 
 @Controller
-@RequestMapping("/sample/*")
+@RequestMapping("/sample")
 @Log4j
 public class SampleController {
 	
@@ -24,6 +24,21 @@ public class SampleController {
 	
 	@GetMapping("/basicOnlyGet")
 	public void basicGet2() {
+		
+		//@GetMapping, @PostMapping, @DeleteMapping, @PutMapping, 
 		log.info("basicGet1 basicOnlyGet URL.......");
+	}
+	
+	
+	//컨트롤러 메소드의 변환타입을 String으로 설정한 경우
+	//해당 스트링은 사용자 브라우저로 전달된 동적 HTML을 생성하는
+	//JSP 파일이름(확장자가 제거된)이름이어야 합니다.
+	//Servlet-context.xml 설명 참고
+	@GetMapping("/ex01")
+	//public String ex01(SampleDTO dto) {
+	public void ex01(SampleDTO dto) {	
+		log.info(""+dto);
+		
+		//return "ex01";
 	}
 }
