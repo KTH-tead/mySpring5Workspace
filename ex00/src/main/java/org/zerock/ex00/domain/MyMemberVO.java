c02a9c669da940720bad440d4db33e2321262494
eddy_goott <kthhyung@gmail.com>
2021-06-10 09:47:39 +0900

    작업

--- ex00/src/main/java/org/zerock/ex00/domain/MyMemberVO.java
+++ ex00/src/main/java/org/zerock/ex00/domain/MyMemberVO.java
@@ -12,7 +12,7 @@ public class MyMemberVO {
 	private String userid;
 	private String userpw;
 	private String userName;
-	private Timestamp mergDate;
+	private Timestamp mregDate;
 	private Timestamp mmodDate; //암호 등 계정 정보 수정 날짜시간
 	private String mdropFlg; //'0'(false) - 유지, '1'(true) - 탈퇴요청
 	private boolean enabled; // 0 (false) - 비활성화 상태, 1 (true) - 활성화 : 휴면계정, 정지상태로 활용//컬럼 데이터유형 CHAR(1)
